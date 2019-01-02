3f8f7a197990537e121f05ebe8b8739e29e3397e
Akash Meshram <30370067+akashmeshram@users.noreply.github.com>
2019-01-02 10:55:20 +0530

    Add files via upload

---
+++ scripts/Main.js
@@ -0,0 +1,54 @@
+let beats;
+let inter;
+
+if (window.matchMedia("(min-width: 768px)").matches) {
+	beats = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+	inter = "16n";
+} else {
+	beats = [0, 1, 2, 3, 4, 5, 6, 7];
+	inter = "8n";
+}
+
+let sequencer = new Nexus.Sequencer('#sequencer', {
+		    'mode': 'toggle',
+		    'rows': 7,
+		    'columns': beats.length,
+		});
+
+sequencer.colorize("accent","#fdcc52");
+sequencer.colorize("fill","#212529");
+
+let percussion = new Nexus.Sequencer('#percussion', {
+		    'mode': 'toggle',
+		    'rows': 4,
+		    'columns': beats.length,
+		});
+percussion.colorize("accent","#fdcc52");
+percussion.colorize("fill","#212529");
+
+let button = new Nexus.Button('#button', {
+		    'mode': 'toggle',
+		    'state': false
+		});
+
+button.colorize("accent","#7d5683");
+button.colorize("fill","#fdcc52");
+button.colorize("mediumLight","#7d5683");
+
+let slider = new Nexus.Slider('#slider', {
+		    'mode': 'relative', 
+		    'min': 80,
+		    'max': 200,
+		    'step': 0,
+		    'value': 120
+		});
+slider.colorize("accent","#7b4397");
+
+let text = new Nexus.Number('#text', {
+		    'value': 6,
+		    'min': 0,
+		    'max': 20000,
+		    'step': 1
+		});
+
+text.link(slider);
---
+++ scripts/Player.js
@@ -0,0 +1,71 @@
+let keys = new Tone.Players({
+    "A": "./audio/casio/A1.[mp3|ogg]",
+    "A#": "./audio/casio/As1.[mp3|ogg]",
+    "B": "./audio/casio/B1.[mp3|ogg]",
+    "C": "./audio/casio/C2.[mp3|ogg]",
+    "C#": "./audio/casio/Cs2.[mp3|ogg]",
+    "D": "./audio/casio/D2.[mp3|ogg]",
+    "D#": "./audio/casio/Ds2.[mp3|ogg]",
+    "E": "./audio/casio/E2.[mp3|ogg]",
+    "F": "./audio/casio/F2.[mp3|ogg]",
+    "F#": "./audio/casio/Fs2.[mp3|ogg]",
+    "G": "./audio/casio/G2.[mp3|ogg]",
+    "G#": "./audio/casio/Gs1.[mp3|ogg]",
+}, {
+    "volume": -10,
+    "fadeOut": "64n",
+}).toMaster();
+
+let steps = new Tone.Players({
+    "hh": "./audio/505/hh.[mp3|ogg]",
+    "hho": "./audio/505/hho.[mp3|ogg]",
+    "kick": "./audio/505/kick.[mp3|ogg]",
+    "snare": "./audio/505/snare.[mp3|ogg]",
+}, {
+    "volume": -10,
+    "fadeOut": "4n",
+}).toMaster();
+
+let notes = ["C", "D", "E", "F", "G", "A", "B"];
+let pers = ["hh", "hho", "kick", "snare"];
+
+let loop = new Tone.Sequence(function(time, col) {
+    let column1 = sequencer.matrix.column(col);
+    let column2 = percussion.matrix.column(col);
+    sequencer.next();
+    percussion.next();
+    for (let i = 0; i < 7; i++) {
+        if (column1[i] === 1) {
+            let vel = Math.random() * 0.5 + 0.5;
+            keys.get(notes[i]).start(time, 0, "4n", 0, vel);
+        }
+    }
+    for (let i = 0; i < 4; i++) {
+        if (column2[i] === 1) {
+            let vel = Math.random() * 0.5 + 0.5;
+            steps.get(pers[i]).start(time, 0, "16n", 0, vel);
+        }
+    }
+}, beats, inter);
+
+Tone.Transport.start();
+
+slider.on('change', function(v) {
+    Tone.Transport.bpm.value = v;
+});
+
+button.on('change', function(v) {
+    if (v) {
+        Tone.context.resume().then(() => {
+            loop.start();
+        });
+    } else {
+        Tone.context.resume().then(() => {
+            sequencer.stepper.value = 15;
+            sequencer.next();
+            percussion.stepper.value = 15;
+            percussion.next();
+            loop.stop();
+        });
+    }
+});
